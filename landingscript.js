70428c058d998729f8217d3ab4f638c5b1a22287
LemonPi <johnson9510@hotmail.com>
2014-11-09 19:06:59 -0500

    Made open landing a function for easy debugging

--- landingscript.js
+++ landingscript.js
@@ -6,11 +6,17 @@ function test_css(classname, test) {
 	return test(meta);
 }
 
+function close_landing() {
+	document.getElementById("google_landing").style.display = "none";
+}
+function open_landing() {
+	document.getElementById("google_landing").style.display = "";
+}
 
 window.onload = function(e) {
-
 	// possiblyGoogler = true;
 	if (typeof(prettyPrint) != "undefined") {possiblyGoogler = true;}
+
 	// public css for testing: http://build.chromium.org/p/chromium/default.css
 	if (test_css("Project", function(meta){return meta.clientWidth == 100;})) {
 		console.log("test passed - chromium buildbot");
@@ -18,11 +24,6 @@ window.onload = function(e) {
 	}
 
 	if (possiblyGoogler) {
-		var landing = document.getElementById("google_landing");
-		landing.style.display = "";
+		open_landing();
 	}
-}
-
-function close_landing() {
-	document.getElementById("google_landing").style.display = "none";
 }

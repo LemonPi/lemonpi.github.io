b4ec790f26fda1c2bd653927ef47b14c811f196b
Johnson Zhong <johnsonzhong@hotmail.ca>
2018-09-03 18:45:36 -0400

    Extend anchor header extension to h3

--- src/extensions.js
+++ src/extensions.js
@@ -1,14 +1,13 @@
 function anchorHeader() {
     const ext = {
         type   : 'output',
-        regex  : /<h2>(.*?)<\/h2>/g,
+        regex  : /<h([23])>(.*?)<\/h\1>/g,
         // regex: /<h2>/g,
-        replace: function (fullMatch, title) {
-            // console.log(title);
+        replace: function (fullMatch, headerLevel, title) {
             // want to convert title to an ID
             const id = title.toLowerCase().replace(" ", "_");
-            return `<h2 class="anchor">${title} <a class="anchor-link" title="permalink to section" href="#${id}"
-                               name="${id}">&para;</a></h2>`;
+            return `<h${headerLevel} class="anchor">${title} <a class="anchor-link" title="permalink to section" href="#${id}"
+                               name="${id}">&para;</a></h${headerLevel}>`;
         }
     };
     return ext;
